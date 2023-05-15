bfda16ea5abc2c6fc9ffc4233996c94a0e9dc78e
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-05-15 17:29:13 +0900

    00_03課題

---
+++ WindowsClass.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdint>
+#include <Windows.h>
+
+class WindowsClass
+{
+public:
+    //ウィンドウサイズ
+    static const int kClientWidth = 1280;
+    static const int kClientHeight = 720;
+
+    void GetWindow(
+        const wchar_t* title = L"CG2", UINT windowStyle = WS_OVERLAPPEDWINDOW,
+        int32_t clientWidth = kClientWidth, int32_t clientHeight = kClientHeight);
+
+    static WindowsClass* GetInstance();
+
+    static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+    HWND hwnd = nullptr;
+
+private:
+
+};
