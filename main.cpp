dca36537f32701825b1e89a40c22e0e06bf87630
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-05-17 15:47:41 +0900

    01_03途中

--- main.cpp
+++ main.cpp
@@ -1,63 +1,19 @@
 #include <Windows.h>
 #include "WindowsClass.h"
+#include "DirectXClass.h"
 #include <string>
 #include <format>
-
-// wstringからstringへ変換する関数
-// string->wstring
-std::wstring ConvertString(const std::string& str)
-{
-	if (str.empty())
-	{
-		return std::wstring();
-	}
-
-	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-	if (sizeNeeded == 0)
-	{
-		return std::wstring();
-	}
-	std::wstring result(sizeNeeded, 0);
-	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-	return result;
-}
-
-// wstring->string
-std::string ConvertString(const std::wstring& str)
-{
-	if (str.empty())
-	{
-		return std::string();
-	}
-
-	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-	if (sizeNeeded == 0)
-	{
-		return std::string();
-	}
-	std::string result(sizeNeeded, 0);
-	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-	return result;
-}
-
-void Log(const std::string& message)
-{
-    OutputDebugStringA(message.c_str());
-}
-
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
     WindowsClass* win = nullptr;
+    DirectXClass* DX = nullptr;
 
     // ゲームウィンドウの作成
     win = WindowsClass::GetInstance();
     win->GetWindow();
 
-
-
-
-	Log("Complete create D3D12Device!!!\n");//初期化完了のログをだす
+    DX = DirectXClass::GetInstance();
 
     MSG msg{};
 
