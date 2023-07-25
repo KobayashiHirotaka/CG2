d944ebccc9fab90577835c0da1393e4d46e8dd77
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-07-25 16:32:58 +0900

    02_00課題

--- main.cpp
+++ main.cpp
@@ -1,41 +1,58 @@
-#include <Windows.h>
-#include "ConvertString.h"
-#include <cstdint>
-#include <format>
-#include <d3d12.h>
-#include <cassert>
-#include "Vector4.h"
-#include "MyEngine.h"
+#include<Windows.h>
+#include"WindowsApp.h"
+#include"DirectXCommon.h"
+#include"MyEngine.h"
+#include"Triangle.h"
 
-// Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	WindowsApp* winApp = new WindowsApp;
+	DirectXCommon* directXCommon = new DirectXCommon;
+	MyEngine* engine = new MyEngine;
 
-	MyEngine* engine = new MyEngine();
-
+	//アプリケーションの開始
+	winApp->StartApp();
+	directXCommon->Initialize(winApp->GetHwnd());
 	engine->Initialize();
 
-#pragma region メインループ
+	Vector4 triangleData[10][3];
+
+	for (int i = 0; i < 4; i++)
+	{
+
+		triangleData[i][0] = { -0.2f,-0.2f + (i * 0.5f),0.0f,1.5f };
+		triangleData[i][1] = { 0.0f,0.2f + (i * 0.4f),0.0f,1.5f };
+		triangleData[i][2] = { 0.2f,-0.2f + (i * 0.5f),0.0f,1.5f };
+	}
+
 
 	MSG msg{};
-	// ウィンドウの×ボタンが押されるまでループ
-	while (msg.message != WM_QUIT) {
-		// Windowにメッセージが来ていたら最優先で処理させる
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+	while (msg.message != WM_QUIT)
+	{
+		//Windowsにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		else {
-			// ループ処理
-			engine->BeginFrame();
+		else
+		{
+			engine->Update();
 
-			engine->Draw();
+			for (int i = 0; i < 4; i++)
+			{
+				//三角形描画
+				engine->Draw(triangleData[i][0], triangleData[i][1], triangleData[i][2]);
+			}
 
-			engine->EndFrame();
+			engine->UpdateEnd();
+
 		}
 	}
-	// 解放処理
-	engine->Release();
-#pragma endregion
 
+	winApp->EndApp();
+	engine->End();
+	directXCommon->PostDraw();
 	return 0;
 }
