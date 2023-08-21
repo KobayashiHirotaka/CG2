a28545da1f3957b573a2e7825de1b76129b810df
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-08-22 05:49:58 +0900

    hlsl,materialの修正

--- Material.h
+++ Material.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "Vector4.h"
+#include "Matrix4x4.h"
 #include <stdint.h>
 
 struct Material
 {
 	Vector4 color;
 	int32_t enableLighting;
+	float padding[3];
+	Matrix4x4 uvTransform;
 };
