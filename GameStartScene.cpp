b2b4d34ae9607204158de4cc7e3f294e47fb539f
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-09-17 17:22:17 +0900

    Start

--- GameStartScene.cpp
+++ GameStartScene.cpp
@@ -1,1 +1,25 @@
-#include "GameStartScene.h"
+#include "GameStartState.h"
+
+void GameStartState::Initialize()
+{
+
+}
+
+void GameStartState::Update()
+{
+	/*time_++;
+	if (time_ >= 60)
+	{
+		stateNum = 1;
+	}*/
+
+	ImGui::Begin("StateNum");
+	ImGui::Text("StateNum %d", stateNum);
+	ImGui::InputInt("SceneNum", &stateNum);
+	ImGui::End();
+}
+
+void GameStartState::Draw()
+{
+
+}
