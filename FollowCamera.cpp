ec5a2f3a1e1bba06b9e617654f2d0825de269b4b
k022g0013@g.neec.ac.jp <k022g0013@g.neec.ac.jp>
2023-10-21 16:39:58 +0900

    FollowCameraの中身書いた

--- FollowCamera.cpp
+++ FollowCamera.cpp
@@ -1,1 +1,34 @@
 #include "FollowCamera.h"
+
+void FollowCamera::Initialize()
+{
+	viewProjection_.Initialize();
+
+	input_ = Input::GetInstance();
+}
+
+void FollowCamera::Update()
+{
+	//追従対象がいれば
+	if (target_)
+	{
+		//追従対象からカメラまでのオフセット
+		Vector3 offset = { 0.0f, 2.0f, -10.0f };
+
+		Matrix4x4 cameraRotateMatrix = MakeRotateMatrix(viewProjection_.rotation);
+
+		offset = TransformNormal(offset, cameraRotateMatrix);
+
+		viewProjection_.translation = Add(target_->translation, offset);
+	}
+
+	if (Input::GetInstance()->GetJoystickState(joyState_))
+	{
+		const float kRotateSpeed = 0.02f;
+
+		viewProjection_.rotation.y += (float)joyState_.Gamepad.sThumbRX / SHRT_MAX * kRotateSpeed;
+	}
+
+	viewProjection_.UpdateViewMatrix();
+	viewProjection_.TransferMatrix();
+}
--- FollowCamera.h
+++ FollowCamera.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "WorldTransform.h"
+#include "ViewProjection.h"
+#include "Input.h"
+#include "MyMath.h"
+
 class FollowCamera
 {
+public:
+	void Initialize();
+
+	void Update();
+
+	const ViewProjection& GetViewProjection() { return viewProjection_; }
+
+	void SetTarget(const WorldTransform* target) { target_ = target; }
+
+private:
+	ViewProjection viewProjection_;
+
+	const WorldTransform* target_ = nullptr;
+
+	Input* input_ = nullptr;
+	XINPUT_STATE joyState_;
 };
 
